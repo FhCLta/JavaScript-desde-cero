878bad24c4e87fc127744a2356214784761e5918
Florencio Hurtado <rafa401128@gmail.com>
2024-08-06 16:53:02 -0500

    Ejercicios.Actualización.

--- Aprende-Javascript/4.Ejercicios de Logica/index.js
+++ Aprende-Javascript/4.Ejercicios de Logica/index.js
@@ -315,6 +315,8 @@ numeroParImpar(6);
 14) Programa una función para convertir grados Celsius a Fahrenheit y viceversa, pe. 
 miFuncion(0,"C") devolverá 32°F.
 */
+
+// 1ra solución
 const convertirGrados = (grados = undefined, unidad = undefined)=>{
     if(grados === undefined) return console.warn("No ingresaste grados a convertir");
 
@@ -342,6 +344,39 @@ convertirGrados(2,"Hola");
 convertirGrados(2,"E");
 convertirGrados(0,"C");
 convertirGrados(100,"F");
+
+// 2da solución
+
+const convertirGrados1 = (grados = undefined, unidad = undefined) => {
+    if (grados === undefined) return console.warn("No ingresaste grados a convertir");
+
+    if (typeof grados !== "number") return console.error(`El valor ${grados} ingresado no es un número`);
+
+    if (unidad === undefined) return console.warn("No ingresaste el tipo de grado a convertir");
+
+    if (typeof unidad !== "string") return console.error(`El valor ${unidad} ingresado no es una cadena de texto`);
+     
+    // se usa otra manera de validad, la más común
+    if (unidad.length !== 1 || (unidad !== "C" && unidad !== "F")) {
+        return console.warn('Valor de unidad no reconocido');
+    }
+
+    if (unidad === "C") {
+        return console.info(`${grados}°C = ${Math.round(grados * (9 / 5) + 32)}°F`);
+    } else if (unidad === "F") {
+        return console.info(`${grados}°F = ${Math.round((grados - 32) * (5 / 9))}°C`);
+    }
+}
+
+convertirGrados1();
+convertirGrados1("2");
+convertirGrados1(2);
+convertirGrados1(2, true);
+convertirGrados1(2, "Hola");
+convertirGrados1(2, "E");
+convertirGrados1(0, "C");
+convertirGrados1(100, "F");
+
 
 
 /*
@@ -375,7 +410,7 @@ pe. miFuncion("Hola Mundo") devuelva Vocales: 4, Consonantes: 5.
 
 
 
- 
+
  /*
 20) Programa una función que valide que un texto sea un email válido,
  pe. miFuncion("jonmircha@gmail.com") devolverá verdadero.
