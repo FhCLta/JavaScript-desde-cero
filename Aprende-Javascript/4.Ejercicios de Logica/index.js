7cb667082ead93f743a257aa179dc6e63b157b22
Florencio Hurtado <rafa401128@gmail.com>
2024-08-11 16:15:46 -0500

    Ejercicios.Actualización

--- Aprende-Javascript/4.Ejercicios de Logica/index.js
+++ Aprende-Javascript/4.Ejercicios de Logica/index.js
@@ -383,16 +383,93 @@ convertirGrados1(100, "F");
 15) Programa una función para convertir números de base binaria a decimal y viceversa, 
 pe. miFuncion(100,2) devolverá 4 base 10.
 */
+ 
+const convertirBinarioDecimal = (numero = undefined, base = undefined) => {
+    if (numero === undefined) return console.warn('No ingresaste el # a convertir');
+
+    if (typeof numero !== "number") return console.error(`El valor ${numero} ingresado, no es un número`);
+
+    if (base === undefined) return console.warn('No ingresaste la base a convertir');
+
+    if (typeof base !== "number") return console.error(`El valor ${base} ingresado, no es un número`);
+
+    if (base === 2) {
+        return console.info(`${numero} base ${base} = ${parseInt(numero, base)} base 10`);
+    }else if(base === 10){
+        return console.info(`${numero} base ${base} = ${(numero.toString(base))} base 2`)
+    }else{
+        return console.error('El tipo de base a convertir No es válido');
+    }
+};
+
+
+convertirBinarioDecimal();
+convertirBinarioDecimal("2");
+convertirBinarioDecimal(100);
+convertirBinarioDecimal(100,"2");
+convertirBinarioDecimal(100,2);
+convertirBinarioDecimal(100,3);
+
+
 
 /*
 16) Programa una función que devuelva el monto final después de aplicar un descuento a una cantidad dada,
  pe. miFuncion(1000, 20) devolverá 800.
  */
 
+ const aplicarDescuento = (monto = undefined, descuento = 0)=>{
+    if(monto === undefined) return console.warn('No ingresaste un #');
+
+    if(typeof monto !== "number") return console.error(`El valor ${monto} ingresado, No es un número`)
+
+    if(monto === 0) return console.error(`El monto no puede ser 0`)
+
+    if(Math.sign(monto) === -1) return console.error('El monto np puede ser 0')
+
+    if(typeof descuento !== "number") return console.error(`El valor ${descuento} ingresado no es un #`)
+        
+    if(Math.sign(descuento) === -1) return console.error('El descuento no puede ser negativo')
+        
+    return console.info(`${monto} - ${descuento}% = ${monto -((monto*descuento)/100)}`)    
+ }
+
+ aplicarDescuento();
+ aplicarDescuento("200");
+ aplicarDescuento(0);
+ aplicarDescuento(-10);
+ aplicarDescuento(1000,"20");
+ aplicarDescuento(1000);
+ aplicarDescuento(1000,20)
+
  /*
 17) Programa una función que dada una fecha válida determine cuantos años han pasado hasta el día de hoy, 
 pe. miFuncion(new Date(1984,4,23)) devolverá 35 años (en 2020).
 */
+
+const calcularAnios=(fecha = undefined)=>{
+    if(fecha === undefined) return console.warn('No ingresaste la fecha')
+
+    if(!(fecha instanceof Date)) return console.error('El valor que ingresaste nos una fecha válida') // me devuelve true si fecha es una instancia de date
+
+    let hoyMenosFecha = new Date().getTime() - fecha.getTime(),
+    aniosEnMS = 1000*60*60 *24*365, // 1000 es 1 segundo para javascrip 
+    aniosHumanos = Math.floor(hoyMenosFecha/aniosEnMS)
+        
+    return (Math.sign(aniosHumanos) === -1)
+    ? console.info(`Faltan ${Math.abs(aniosHumanos)} años para el ${fecha.getFullYear()}`)
+    : (Math.sign(aniosHumanos) === 1)
+    ? console.info(`Han pasado ${aniosHumanos} años, desde ${fecha.getFullYear()} `)
+    : console.info(`Esramos en el año actual ${fecha.getFullYear()}`)  
+}
+
+calcularAnios();
+calcularAnios({});
+calcularAnios(false);
+calcularAnios(new Date());
+calcularAnios(new Date(1995,9,24));
+calcularAnios(new Date(2100,9,24));
+
+
 
 /*
 18) Programa una función que dada una cadena de texto cuente el número de vocales y consonantes, 
