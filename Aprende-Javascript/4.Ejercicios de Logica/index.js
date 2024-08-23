f6232a5a6ac9f0549a57a91f7e452cd44f74a3e8
Florencio Hurtado <rafa401128@gmail.com>
2024-08-23 13:06:05 -0500

    Ejercicios.actualización

--- Aprende-Javascript/4.Ejercicios de Logica/index.js
+++ Aprende-Javascript/4.Ejercicios de Logica/index.js
@@ -704,15 +704,72 @@ ordenarArreglo([7,5,7,8,6])
 elimine los duplicados, pe. miFuncion(["x", 10, "x", 2, "10", 10, true, true])
  devolverá ["x", 10, 2, "10", true].
  */
+ 
+const quitarDuplicados = (arr = undefined) => {
+    if(arr === undefined) return console.warn("No ingresaste un arreglo de numeros")
 
+    if(!(arr instanceof Array)) return console.error("El arreglo está vacío");
+        
+    if(arr.length === 0) return console.error("EL arreglo está vacío");
 
+    if(arr.length === 1) return console.error("EL arreglo está vacío");
 
+   /* // 1ra solución
+   return console.info({
+    original: arr,
+    sinDuplicados: arr.filter((value,index,self) => self.indexOf(value) === index)
+   }) */
+    return console.info({
+      original: arr,
+      sinDuplicados: [...new Set(arr)]
+       })
+
+   }
+
+  quitarDuplicados();
+  quitarDuplicados({});
+  quitarDuplicados([]);
+  quitarDuplicados( ["x", 10,"x", 2, "10", true]);
 
 
  /*
 26) Programa una función que dado un arreglo de números obtenga el promedio, 
 pe. promedio([9,8,7,6,5,4,3,2,1,0]) devolverá 4.5.
 */
+
+const promedio = (arr = undefined) =>{
+
+    if(arr === undefined) return console.warn("No ingresaste un arreglo de numeros")
+
+    if(!(arr instanceof Array)) return console.error("El arreglo está vacío");
+            
+    if(arr.length === 0) return console.error("EL arreglo está vacío");
+    
+    for(let num of arr){
+        if(typeof num !== "number") return console.error(`El valor ${num} ingresado , no es un número`);
+
+      return console.info(
+        arr.reduce((total,num,index,arr) =>{
+        
+            total += num;
+            if(index === arr.length - 1) {
+                return `El promedio de ${arr.join("+")} es ${total / arr.length}`;  
+            } else {
+                return total;
+            }
+        })
+      )
+}
+}
+
+promedio();
+promedio({});
+promedio([]);
+promedio([2,true]);
+promedio([1,2,3,4,5,6,7,8,9])
+
+
+
 
 /*
 27) Programa una clase llamada Pelicula.
